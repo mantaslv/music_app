f094822bc2a30820786bb4f0ecd208811d7e9368
mantaslv <mantasvolkas@gmail.com>
2023-06-05 13:40:55 +0100

    randomNumber.py created

---
+++ makers/phase-one/randomNumber.py
@@ -0,0 +1,14 @@
+import random
+
+random_number = random.randint(1, 5)
+
+# Print "One" if `random_number` is 1
+# Print "Two" if `random_number` is 2
+# Print "More" if `random_number is above 2
+
+if random_number == 1:
+    print("One")
+elif random_number == 2:
+    print("Two")
+else:
+    print("More")
