d64e4bd46fb92c6a86e7ba46de1aa77a02529513
mantaslv <mantasvolkas@gmail.com>
2023-06-05 17:49:45 +0100

    FizzBuzz works up to 4

---
+++ makers/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,15 @@
+# For: fizzbuzz.py
+def generate(upto):
+    my_list = []
+    for num in range(1, upto + 1):
+        if num % 3 == 0:
+            my_list.append("Fizz")
+        else:
+            my_list.append(str(num))
+
+
+        
+
+    result = ", ".join(my_list)
+
+    return result
---
+++ makers/fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,21 @@
+# For: test_fizzbuzz.py
+import unittest  # This is the default Python testing library, unittest
+
+from fizzbuzz import generate
+# We import our generate function (def) from the fizzbuzz module (file)
+
+class TestFizzbuzz(unittest.TestCase):  # Sets up a new test case
+    def test_lists_values_up_to_one(self):  # This is a test, don't forget `self`!
+        self.assertEqual(generate(1), "1")  # And an assertion
+
+    def test_lists_values_up_to_two(self):
+        self.assertEqual(generate(2), "1, 2")
+
+    def test_lists_values_up_to_three(self):
+        self.assertEqual(generate(3), "1, 2, Fizz")
+
+    def test_lists_values_up_to_four(self):
+        self.assertEqual(generate(4), "1, 2, Fizz, 4")
+
+    # Start by uncommenting the above, and when you've made that pass move 
+    # forward with your own tests.
