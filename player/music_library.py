733e96aaddf827165afedc3012c870acaeb6a4cf
mantaslv <mantasvolkas@gmail.com>
2023-06-11 17:19:52 +0100

    Started tdd again with db, music lib constructs

--- player/music_library.py
+++ player/music_library.py
@@ -11,67 +11,69 @@ class Track:
         return f"{self.title} by {self.artist}"
 
 class MusicLibrary:
-    def __init__(self, env='production'):
-        self._tracks = []
-        self._connection = None
-        self._cursor = None
-        self.env = env
+    def __init__(self):
+        self.db_name = "python_music"
+        self.cursor = None
+        self.connect()
 
-        self._connection = psycopg2.connect(
-            host="127.0.0.1",
-            database="python_music" if env == 'production' else "python_music_test"
+    def connect(self):
+        self.conn = psycopg2.connect(
+            database=self.db_name,
         )
-        self._cursor = self._connection.cursor()
-        self._create_table()
+        self.cursor = self.conn.cursor()
 
-    def _create_table(self):
-        create_table_query = '''
-        CREATE TABLE IF NOT EXISTS tracks (
-            id SERIAL PRIMARY KEY,
-            title VARCHAR,
-            artist VARCHAR,
-            file VARCHAR
-        )
-        '''
-        self._cursor.execute(create_table_query)
-        self._connection.commit()
-    
+    def create_table(self):
+        query = """
+            CREATE TABLE IF NOT EXISTS tracks (
+                id SERIAL PRIMARY KEY,
+                title TEXT NOT NULL,
+                artist TEXT NOT NULL,
+                file TEXT NOT NULL
+            )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
     def all(self):
-        select_query = 'SELECT title, artist, file FROM tracks'
-        self._cursor.execute(select_query)
-        tracks = self._cursor.fetchall()
-        return [Track(*track) for track in tracks]
-    
-    def add(self, song):
-        insert_query = 'INSERT INTO tracks (title, artist, file) VALUES (%s, %s, %s) RETURNING id'
-        values = (song.title, song.artist, song.file)
-        self._cursor.execute(insert_query, values)
-        song_id = self._cursor.fetchone()[0]
-        self._connection.commit()
-        song.id = song_id
+        query = 'SELECT title, artist, file FROM tracks'
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
 
-    def remove(self, song_id):
-        delete_query = 'DELETE FROM tracks WHERE id = %s'
-        values = (song_id,)
-        self._cursor.execute(delete_query, values)
-        self._connection.commit()
-        return self._cursor.rowcount > 0
+    # def all(self):
+    #     select_query = 'SELECT title, artist, file FROM tracks'
+    #     tracks = DatabaseConnection.exec_params(select_query)
+    #     if not tracks:
+    #         return []
+    #     return [Track(title, artist, file) for title, artist, file in tracks]
 
-    def search(self, condition):
-        select_query = 'SELECT title, artist, file FROM tracks'
-        self._cursor.execute(select_query)
-        tracks = self._cursor.fetchall()
-        return [Track(*track) for track in tracks if condition(Track(*track))]
+    # def add(self, song):
+    #     insert_query = 'INSERT INTO tracks (title, artist, file) VALUES (%s, %s, %s) RETURNING id'
+    #     values = (song.title, song.artist, song.file)
+    #     song_id = DatabaseConnection.exec_params(insert_query, values)[0][0]
+    #     song.id = song_id
 
-    def tally(self):
-        select_query = 'SELECT artist, COUNT(*) FROM tracks GROUP BY artist'
-        self._cursor.execute(select_query)
-        tally_rows = self._cursor.fetchall()
-        tally = {artist: count for artist, count in tally_rows}
+    # def remove(self, song_id):
+    #     delete_query = 'DELETE FROM tracks WHERE id = %s'
+    #     values = (song_id,)
+    #     rows_deleted = DatabaseConnection.exec_params(delete_query, values)
+    #     return rows_deleted > 0
 
-        return tally
+    # def search(self, condition):
+    #     select_query = 'SELECT title, artist, file FROM tracks'
+    #     tracks = DatabaseConnection.exec_params(select_query)
+    #     return [Track(*track) for track in tracks if condition(Track(*track))]
 
-    def __del__(self):
-        if self.env == 'production':
-            self._cursor.close()
-            self._connection.close()
+    # def tally(self):
+    #     select_query = 'SELECT artist, COUNT(*) FROM tracks GROUP BY artist'
+    #     tally_rows = DatabaseConnection.exec_params(select_query)
+    #     tally = {artist: count for artist, count in tally_rows}
+
+    #     return tally
+
+    # def __del__(self):
+    #     if self.env == 'production':
+    #         self._connection.close()
--- tests/test_music_library.py
+++ tests/test_music_library.py
@@ -3,87 +3,82 @@ import psycopg2
 from player.music_library import MusicLibrary, Track
 
 class TestMusicLibrary(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Connect to the test database
-        cls.connection = psycopg2.connect(
-            host="127.0.0.1",
-            database="python_music_test"
-        )
-        cls.cursor = cls.connection.cursor()
-        cls.music_library = MusicLibrary(env='test')
-
-    @classmethod
-    def tearDownClass(cls):
-        # Clean up the test database and close the connection
-        cls.cursor.close()
-        cls.connection.close()
-
     def setUp(self):
-        # Clear any existing data in the test database before each test
-        self.cursor.execute("DELETE FROM tracks")
-        self.connection.commit()
+        self.lib = MusicLibrary()
+        self.lib.db_name = "python_music_test"
+        self.lib.connect()
+        self.lib.create_table()
+        self.music_lib = MusicLibrary()
+
+    def tearDown(self):
+        self.lib.close()
+        conn = psycopg2.connect(database="python_music_test")
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM tracks")
+        conn.commit()
+        cursor.close()
+        conn.close()
 
     def test_constructs(self):
-        self.assertIsInstance(self.music_library, MusicLibrary)
+        self.assertIsInstance(self.music_lib, MusicLibrary)
 
-    def test_returns_empty_all(self):
-        self.assertEqual(self.music_library.all(), [])
+    # def test_returns_empty_all(self):
+    #     self.assertEqual(self.music_lib.all(), [])
 
-    def test_adds_one(self):
-        track = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
-        self.music_library.add(track)
-        all_tracks = self.music_library.all()
-        self.assertEqual(all_tracks[0].artist, track.artist)
+    # def test_adds_one(self):
+    #     track = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
+    #     self.music_library.add(track)
+    #     all_tracks = self.music_library.all()
+    #     self.assertEqual(all_tracks[0].artist, track.artist)
 
-    def test_adds_multiple(self):
-        track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
-        track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
-        track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
-        self.music_library.add(track1)
-        self.music_library.add(track2)
-        self.music_library.add(track3)
-        self.assertEqual(len(self.music_library.all()), 3)
-        self.assertEqual(self.music_library.all()[2].artist, track3.artist)
+    # def test_adds_multiple(self):
+    #     track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
+    #     track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
+    #     track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
+    #     self.music_library.add(track1)
+    #     self.music_library.add(track2)
+    #     self.music_library.add(track3)
+    #     self.assertEqual(len(self.music_library.all()), 3)
+    #     self.assertEqual(self.music_library.all()[2].artist, track3.artist)
 
-    def test_removes_existing_song(self):
-        track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
-        track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
-        track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
-        self.music_library.add(track1)
-        self.music_library.add(track2)
-        self.music_library.add(track3)
-        self.assertTrue(self.music_library.remove(track2.id))
-        self.assertEqual(len(self.music_library.all()), 2)
-        self.assertEqual(self.music_library.all()[0].title, track1.title)
-        self.assertEqual(self.music_library.all()[1].title, track3.title)
+    # def test_removes_existing_song(self):
+    #     track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
+    #     track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
+    #     track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
+    #     self.music_library.add(track1)
+    #     self.music_library.add(track2)
+    #     self.music_library.add(track3)
+    #     self.assertTrue(self.music_library.remove(track2.id))
+    #     self.assertEqual(len(self.music_library.all()), 2)
+    #     self.assertEqual(self.music_library.all()[0].title, track1.title)
+    #     self.assertEqual(self.music_library.all()[1].title, track3.title)
 
-    def test_cant_remove_nonexistent_song(self):
-        track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
-        track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
-        track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
-        self.music_library.add(track1)
-        self.music_library.add(track2)
-        self.music_library.add(track3)
-        self.assertFalse(self.music_library.remove(20))
-        self.assertFalse(self.music_library.remove(track3.id + 1))
-        self.assertEqual(len(self.music_library.all()), 3)
+    # def test_cant_remove_nonexistent_song(self):
+    #     track1 = Track(title="Rolling Blackouts", artist="The Go! Team", file="file1.mp3")
+    #     track2 = Track(title="Oh Yeah", artist="Locust", file="file2.mp3")
+    #     track3 = Track(title="Sleep on the Wing", artist="Bibio", file="file3.mp3")
+    #     self.music_library.add(track1)
+    #     self.music_library.add(track2)
+    #     self.music_library.add(track3)
+    #     self.assertFalse(self.music_library.remove(20))
+    #     self.assertFalse(self.music_library.remove(track3.id + 1))
+    #     self.assertEqual(len(self.music_library.all()), 3)
 
-    def test_search(self):
-        track1 = Track(title="Dead Letters", artist="P.S. Eliot", file="dl.mp3")
-        track2 = Track(title="Friend Is A Four Letter Word", artist="CAKE", file="friend.mp3")
-        track3 = Track(title="Letters '98", artist="Havergal", file="98.mp3")
-        self.music_library.add(track1)
-        self.music_library.add(track2)
-        self.music_library.add(track3)
-        result = self.music_library.search(lambda track: "dead" in track.title.lower())
-        self.assertEqual(result[0].title, track1.title)
+    # def test_search(self):
+    #     track1 = Track(title="Dead Letters", artist="P.S. Eliot", file="dl.mp3")
+    #     track2 = Track(title="Friend Is A Four Letter Word", artist="CAKE", file="friend.mp3")
+    #     track3 = Track(title="Letters '98", artist="Havergal", file="98.mp3")
+    #     self.music_library.add(track1)
+    #     self.music_library.add(track2)
+    #     self.music_library.add(track3)
+    #     result = self.music_library.search(lambda track: "dead" in track.title.lower())
+    #     self.assertEqual(result[0].title, track1.title)
 
-    def test_tally(self):
-        track1 = Track(title="Mixed Emotions", artist="Netsky", file="file2.mp3")
-        track2 = Track(title="Where Do We Go", artist="Dimension", file="file1.mp3")
-        track3 = Track(title="DJ Turn It Up", artist="Dimension", file="file3.mp3")
-        self.music_library.add(track1)
-        self.music_library.add(track2)
-        self.music_library.add(track3)
-        self.assertEqual(self.music_library.tally(), {'Dimension': 2, 'Netsky': 1})
+    # def test_tally(self):
+    #     track1 = Track(title="Mixed Emotions", artist="Netsky", file="file2.mp3")
+    #     track2 = Track(title="Where Do We Go", artist="Dimension", file="file1.mp3")
+    #     track3 = Track(title="DJ Turn It Up", artist="Dimension", file="file3.mp3")
+    #     self.music_library.add(track1)
+    #     self.music_library.add(track2)
+    #     self.music_library.add(track3)
+    #     self.assertEqual(self.music_library.tally(), {'Dimension': 2, 'Netsky': 1})
